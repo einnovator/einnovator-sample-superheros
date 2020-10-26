184dff499d934edb3dbafc5e55286ad3a725207c
Jorge Simao <jsimao71@gmail.com>
2020-10-26 17:19:14 +0000

    fix style

--- src/main/java/org/einnovator/sample/superheros/SuperherosApplication.java
+++ src/main/java/org/einnovator/sample/superheros/SuperherosApplication.java
@@ -1,12 +1,15 @@
 package org.einnovator.sample.superheros;
 
+import org.einnovator.sample.superheros.config.AppConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class SuperherosApplication {
 
 	public static void main(String[] args) {
+		new SpringApplicationBuilder(SuperherosApplication.class).profiles(AppConfig.getProfiles()).build(args);
 		SpringApplication.run(SuperherosApplication.class, args);
 	}
 	
